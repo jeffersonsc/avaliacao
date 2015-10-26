c56ad1edb27c4e88a72dbfba0928d84a1bcad3e9
Jefferson Cunha <jeffersondasilva14@gmail.com>
2015-10-25 23:43:29 -0200

    Adicionado botstrap e ajustes visuais de views
    Signed-off-by: Jefferson Cunha <jeffersondasilva14@gmail.com>

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,10 @@
 class OrdersController < ApplicationController
   def index
-  	@orders = Order.all
-  	@total = 0.0
+  	@orders = Order.paginate(:page => params[:page], :per_page => 30)
+  	@total = [0.0, 0]
+  	Order.all.map{ |order| @total[0] += (order.amount.to_f * order.unit_price.to_f); @total[1] += 1 }
+  	@total_customers = Customer.count
+  	@total_products = Product.count
+  	@total_providers = Provider.count
   end
 end
--- app/helpers/files_helper.rb
+++ app/helpers/files_helper.rb
@@ -1,5 +1,5 @@
 module FilesHelper
 	def processed?(value)
-		value == true ? "Processado" : "Processando"
+		value == true ? "Processado" : image_tag("progress.gif", size: "150x30")
 	end
 end
